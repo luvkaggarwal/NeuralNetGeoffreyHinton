5d28280e041cd2cba531198eefae3c3051deac50
Luv Aggarwal <luvkaggarwal@gmail.com>
2017-07-23 12:18:17 +0530

    Assingment 4 completed

--- Assignment 4/cd1.m
+++ Assignment 4/cd1.m
@@ -2,5 +2,11 @@ function ret = cd1(rbm_w, visible_data)
 % <rbm_w> is a matrix of size <number of hidden units> by <number of visible units>
 % <visible_data> is a (possibly but not necessarily binary) matrix of size <number of visible units> by <number of data cases>
 % The returned value is the gradient approximation produced by CD-1. It's of the same shape as <rbm_w>.
-    error('not yet implemented');
+    visible_data = sample_bernoulli(visible_data);
+    hidden_data = sample_bernoulli(visible_state_to_hidden_probabilities(rbm_w,visible_data));
+    visible_reconstruction = sample_bernoulli(hidden_state_to_visible_probabilities(rbm_w,hidden_data));
+    hidden_reconstruction = visible_state_to_hidden_probabilities(rbm_w,visible_reconstruction);
+    data_goodness = configuration_goodness_gradient(visible_data,hidden_data);
+    reconstruction_goodness = configuration_goodness_gradient(visible_reconstruction,hidden_reconstruction);
+    ret = data_goodness .- reconstruction_goodness;
 end
--- Assignment 4/configuration_goodness.m
+++ Assignment 4/configuration_goodness.m
@@ -3,5 +3,13 @@ function G = configuration_goodness(rbm_w, visible_state, hidden_state)
 % <visible_state> is a binary matrix of size <number of visible units> by <number of configurations that we're handling in parallel>.
 % <hidden_state> is a binary matrix of size <number of hidden units> by <number of configurations that we're handling in parallel>.
 % This returns a scalar: the mean over cases of the goodness (negative energy) of the described configurations.
-    error('not yet implemented');
+    G = 0;
+    for i = 1:size(visible_state,2)
+    	for j = 1:size(rbm_w,1)
+    		for k = 1:size(rbm_w,2)
+    			G += rbm_w(j,k) * visible_state(k,i) * hidden_state(j,i);
+    		endfor
+    	endfor
+    endfor
+    G /= size(visible_state,2);
 end
